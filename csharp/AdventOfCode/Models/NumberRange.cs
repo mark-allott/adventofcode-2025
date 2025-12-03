005aadb883b35b198b18df97569ba35f571d06d7
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-12-03 11:40:27 +0000

    Add model class to yield all integral numbers within given ranges

---
+++ csharp/AdventOfCode/Models/NumberRange.cs
@@ -0,0 +1,40 @@
+namespace AdventOfCode.Models
+{
+	public record NumberRange
+	{
+		public IEnumerable<long> Numbers
+		{
+			get
+			{
+				for (var i = Start; i <= End; i++)
+					yield return i;
+			}
+		}
+
+		public long Start { get; init; }
+		public long End { get; init; }
+
+		public NumberRange(long start, long end)
+		{
+			Start = start;
+			End = end;
+		}
+
+		public NumberRange(string range)
+		{
+			ArgumentException.ThrowIfNullOrWhiteSpace(range);
+			var parts = range.Split('-');
+			ArgumentOutOfRangeException.ThrowIfNotEqual(parts.Length, 2, nameof(range));
+			Start = long.Parse(parts[0]);
+			End = long.Parse(parts[1]);
+		}
+
+#if DEBUG
+		public override string ToString()
+		{
+			return $"Start: {Start}, End: {End}, Count={1 + End - Start}";
+		}
+
+#endif
+	}
+}
