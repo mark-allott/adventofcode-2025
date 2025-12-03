485bad1db620d083fd7db2ae47a29cc11b37be06
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-12-03 11:46:04 +0000

    Refactor: change expected to const value

--- csharp/AdventOfCode/Challenges/Day02/Day02.p1.cs
+++ csharp/AdventOfCode/Challenges/Day02/Day02.p1.cs
@@ -19,7 +19,7 @@ namespace AdventOfCode.Challenges.Day02
 		void IPartOneTest.Test()
 		{
 			var actual = SolvePartOne(_partOneTestData);
-			Debug.Assert(actual == _partOneExpected);
+			Debug.Assert(actual == PartOneExpected);
 		}
 
 		private static long SolvePartOne(List<string> input)
@@ -57,6 +57,6 @@ namespace AdventOfCode.Challenges.Day02
 			"11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
 		];
 
-		private readonly long _partOneExpected = 1227775554;
+		private const long PartOneExpected = 1227775554;
 	}
 }
