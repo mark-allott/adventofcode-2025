91a2b1894aedb4c2a8e230f03b5a752d5eec9779
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-12-07 11:24:56 +0000

    Add alternate constructor to take inputs for location and character, converting to correct types

--- csharp/AdventOfCode/Models/TachyonSplitterCell.cs
+++ csharp/AdventOfCode/Models/TachyonSplitterCell.cs
@@ -1,9 +1,14 @@
 using AdventOfCode.Enums;
+using AdventOfCode.Extensions;
 
 namespace AdventOfCode.Models
 {
 	public record TachyonSplitterCell
 		: Cell<TachyonSplitterState, GridCoordinate, int>
 	{
+		public TachyonSplitterCell(int row, int column, char state)
+			: base(new GridCoordinate(column, row) { X = column, Y = row }, state.ToState())
+		{
+		}
 	}
 }
