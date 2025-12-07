b88c7f9c8484705f7c98054d934688a67a7ab887
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2025-12-07 11:09:24 +0000

    Add record for a Cell in a given location and state

---
+++ csharp/AdventOfCode/Models/Cell.cs
@@ -0,0 +1,45 @@
+using AdventOfCode.Interfaces;
+
+namespace AdventOfCode.Models
+{
+	public record Cell<TState, TCoord, TCoordType>
+		: ICell<TState, TCoord>
+		where TState : notnull
+		where TCoord : ICoordinate<TCoordType>
+		where TCoordType : struct, IComparable<TCoordType>, IEquatable<TCoordType>
+	{
+		#region Constructors
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public Cell()
+		{
+		}
+
+		/// <summary>
+		/// Standard constructor
+		/// </summary>
+		/// <param name="location">The cell location</param>
+		/// <param name="state">The cell state</param>
+		public Cell(TCoord location, TState state)
+		{
+			Location = location;
+			State = state;
+		}
+
+		#endregion
+
+		#region ICell implementation
+
+		/// <inheritdoc/>
+		public required TCoord Location { get; init; }
+
+		/// <summary>
+		/// The state of the cell - could be an enum value, character, etc.
+		/// </summary>
+		public required TState State { get; set; }
+
+		#endregion
+	}
+}
