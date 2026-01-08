2cb71429608fe7652f73dfa7526d715e638f4112
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2026-01-08 17:50:40 +0000

    Extension method that calculates the area enclosed by two coordinates

---
+++ csharp/AdventOfCode/Extensions/CoordinateExtensions.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCode.Models;
+
+namespace AdventOfCode.Extensions
+{
+	internal class CoordinateExtensions
+	{
+		public static long AreaEnclosedBy(Coordinate<int> coordA, Coordinate<int> coordB)
+		{
+			long dx = 1 + Math.Abs(coordA.X - coordB.X);
+			long dy = 1 + Math.Abs(coordA.Y - coordB.Y);
+			return dx * dy;
+		}
+	}
+}
