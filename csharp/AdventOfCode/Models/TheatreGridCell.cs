185836d78c72b0ea8954231db0710d82f2134169
Mark Allott <47857898+mark-allott@users.noreply.github.com>
2026-01-12 16:55:57 +0000

    Define a grid cell type for the Theatre

---
+++ csharp/AdventOfCode/Models/TheatreGridCell.cs
@@ -0,0 +1,13 @@
+﻿using AdventOfCode.Enums;
+
+namespace AdventOfCode.Models
+{
+	internal record TheatreGridCell
+		: Cell<TileColour, TheatreCoordinate, int>
+	{
+		public TheatreGridCell(TheatreCoordinate location, TileColour state)
+			: base(location, state)
+		{
+		}
+	}
+}
